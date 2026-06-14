e11033618d9d47709b7599e4690d03426ad1fe7b
Kshitij Natu <kshitij.natu@gmail.com>
2026-06-14 13:54:41 -0400

    TypeScript APIs created

---
+++ typescript-express-CRUD/src/Student.ts
@@ -0,0 +1,11 @@
+// src/types.ts
+export interface Student {
+    id: number;
+    name: string;
+    email: string;
+    student_id: string;
+    major: string;
+    year: string;
+    gpa: number;
+    enrollment_date: string;
+  }
